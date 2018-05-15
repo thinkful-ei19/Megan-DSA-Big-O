9d4255c8ac794f05fb34d98ff1461b6436f292a9
leonardom249 <leonardo.megan@gmail.com>
2018-05-15 11:27:50 -0400

    completed even or odd

--- even-or-odd.js
+++ even-or-odd.js
@@ -8,3 +8,6 @@ function isEven(value){
     return false;
 }
 
+//this is a Constant Time or O(1) because it is only looking at 1 value/input
+//it is either true or false no matter what and it doesn't loop through anything
+//aka it can either be divided by 2 or not. That's it.
